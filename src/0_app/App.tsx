ac55f6dde658e1b86eb54ea49c712b532d55ad63
Egor Badulin <nobodykenny@gmail.com>
2023-11-17 04:00:26 +0300

    feat: add Container component

--- src/0_app/App.tsx
+++ src/0_app/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import cls from './App.module.scss'
 import classNames from "classnames";
 import {useTheme} from "5_shared/libs/hooks/useTheme";
 import {AppRouter} from "./prodivers/router";
 import {Footer} from "2_widgets/Footer";
+import {Header} from "2_widgets/Header";
 
 const App = () => {
     const { theme} = useTheme();
 
     return (
         <div className={classNames(cls.app, cls[`app--${theme}`])}>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/list/'}>Список</Link>
+            <Header
+                className={classNames(cls.header)}
+            />
             <main className={classNames(cls.main)}>
                 <AppRouter/>
             </main>
