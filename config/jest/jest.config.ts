e0b448571d7901609799f709dcb1c8ccc2784ffc
Egor Badulin <nobodykenny@gmail.com>
2023-11-19 02:50:57 +0300

    feat: add React-Testing-Library

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -193,6 +193,10 @@ const config: Config = {
 
     // Whether to use watchman for file crawling
     // watchman: true,
+
+    modulePaths: [
+        '<rootDir>src',
+    ],
 };
 
 export default config;
---
+++ src/5_shared/ui/Container/Container.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import { Container } from './Container';
+
+describe('Container', () => {
+    test('Container is rendered', () => {
+        render(<Container>TEST</Container>);
+    });
+});
