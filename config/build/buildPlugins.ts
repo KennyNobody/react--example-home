f61c19bee26781bab736d8b484809cf1fceec555
Egor Badulin <nobodykenny@gmail.com>
2023-11-20 19:10:22 +0300

    feat: edit webpack plugins config

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,12 +1,12 @@
 import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BuildMode, BuildOptions } from './types/config';
+import {BuildMode, BuildOptions} from './types/config';
 
 export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstance[] {
     const { paths } = options;
 
-    return [
+    const plugins: webpack.WebpackPluginInstance[] = [
         new HTMLWebpackPlugin({
             template: paths.html,
         }),
@@ -18,6 +18,11 @@ export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstan
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(options.mode === BuildMode.DEV),
         }),
-        new webpack.HotModuleReplacementPlugin(),
     ];
+
+    if (options.mode === BuildMode.DEV) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+    }
+
+    return plugins;
 }
