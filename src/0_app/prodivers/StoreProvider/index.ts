56f3051a0309fedc98271c025d71082d71223d30
Egor Badulin <nobodykenny@gmail.com>
2023-12-01 09:54:21 +0300

    feat: edit config redux

---
+++ src/0_app/prodivers/StoreProvider/index.ts
@@ -0,0 +1,9 @@
+export {
+    StoreProvider,
+} from './ui/StoreProvider';
+
+export {
+    createReduxStore,
+} from './config/store';
+
+export type { StateSchema } from './config/StateSchema';
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from '0_app/App';
 import '0_app/styles/index.scss';
+import { StoreProvider } from '0_app/prodivers/StoreProvider';
 import { ErrorBoundary } from '0_app/prodivers/ErrorBoundary';
 import ThemeProvider from '0_app/prodivers/theme/ThemeProvider';
 import '5_shared/config/i18n/i18n';
@@ -11,12 +12,14 @@ const container = document.getElementById('root');
 if (container) {
     const root = createRoot(container);
     root.render(
-        <BrowserRouter>
-            <ErrorBoundary>
-                <ThemeProvider>
-                    <App />
-                </ThemeProvider>
-            </ErrorBoundary>
-        </BrowserRouter>,
+        <StoreProvider>
+            <BrowserRouter>
+                <ErrorBoundary>
+                    <ThemeProvider>
+                        <App />
+                    </ThemeProvider>
+                </ErrorBoundary>
+            </BrowserRouter>
+        </StoreProvider>,
     );
 }
